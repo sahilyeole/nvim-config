be11fbc97d0a511c3d237dea1465b674af9ac5b1
Sahil Yeole <sahilyeole93@gmail.com>
2023-03-31 23:58:39 +0530

    add highlight to yanked

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 require("sahil")
 vim.cmd('autocmd VimEnter * NvimTreeToggle')
-
+vim.api.nvim_exec([[
+  augroup highlight_yank
+  autocmd!
+  autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup END
+]], false)
